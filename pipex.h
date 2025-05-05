8ea9addfbadfd308cecf319c3b10def2d16af486
adamoski13 <adamjurczyk92@gmail.com>
2025-05-05 16:06:16 +0200

    Update pipex.h

--- pipex.h
+++ pipex.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajurczyk <ajurczyk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 17:03:00 by ajurczyk          #+#    #+#             */
+/*   Updated: 2025/05/05 16:03:45 by ajurczyk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PIPEX_H
 # define PIPEX_H
 # include <unistd.h>
@@ -8,11 +20,14 @@
 # include "libft/libft.h"
 # include <fcntl.h>
 
-char    *get_ep_path(char **ep, char *cmd);
-int	    open_file(char *file, int in_or_out);
-void    do_read(int *pipe1, char **av, char **envp);
-void    do_write(int *pipe1, char **av, char **envp);
+char	*get_ep_path(char **ep, char *cmd);
+char	*return_path(char **env_paths, char *cmd);
+int		open_file(char *file, int in_or_out);
+void	do_read(int *pipe1, char **av, char **envp);
+void	do_write(int *pipe1, char **av, char **envp);
 void	wrong_command(char *s);
 void	wrong_file(char *s);
+void	check_ac_av(int ac, char **av);
+int		check_command(char **ep, char *cmd);
 
 #endif
