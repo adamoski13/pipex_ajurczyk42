5e03b9a679a02f27befe009312b42cb0e6fc97f2
adamoski13 <adamjurczyk92@gmail.com>
2025-05-05 16:04:38 +0200

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,25 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajurczyk <ajurczyk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 16:57:51 by ajurczyk          #+#    #+#             */
+/*   Updated: 2025/05/05 16:03:44 by ajurczyk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipex.h"
 
 void	wrong_file(char *s)
 {
-    if (access(s, X_OK) != 0)
-    {
-        ft_putstr_fd("pipex: permission denied: ", STDERR_FILENO);
-        ft_putstr_fd(s, STDERR_FILENO);
-        ft_putstr_fd("\n", STDERR_FILENO);
-    } else
-    {
-	    ft_putstr_fd("pipex: no such file or directory :", STDERR_FILENO);
-        ft_putstr_fd(s, STDERR_FILENO);
-        ft_putstr_fd("\n", STDERR_FILENO);
-    }
+	if (access(s, X_OK) != 0)
+	{
+		ft_putstr_fd("pipex: permission denied: ", STDERR_FILENO);
+		ft_putstr_fd(s, STDERR_FILENO);
+		ft_putstr_fd("\n", STDERR_FILENO);
+	}
+	else
+	{
+		ft_putstr_fd("pipex: no such file or directory :", STDERR_FILENO);
+		ft_putstr_fd(s, STDERR_FILENO);
+		ft_putstr_fd("\n", STDERR_FILENO);
+	}
 }
 
 void	wrong_command(char *s)
 {
-	ft_putstr_fd("pipex: ", STDERR_FILENO);
-	ft_putstr_fd("command not found :", STDERR_FILENO);
-    ft_putstr_fd(s, STDERR_FILENO);
-    ft_putstr_fd("\n", STDERR_FILENO);
-	exit(1);
+	ft_putstr_fd("pipex: command not found :", STDERR_FILENO);
+	ft_putstr_fd(s, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
 }
